5da538faf62ba83251ad1a1c33bc60416bed5e92
decapo01 <decapo01@gmail.com>
2019-12-22 00:39:00 -0700

    Added a custom error

--- src/Avtor.hs
+++ src/Avtor.hs
@@ -14,17 +14,30 @@ import           Control.Monad.Trans.Except
 import           Control.Monad
 import           Data.Time (UTCTime)
 
-type CreateAccount = CreateAccountReq -> IO (Either Text Account)
-type UpdateAccount = UpdateAccountReq -> IO (Either Text Account)
+type CreateAccount = CreateAccountReq -> IO (Either AvtorError Account)
+type UpdateAccount = UpdateAccountReq -> IO (Either AvtorError Account)
 
-type RegisterUser = RegisterReq -> IO (Either Text UnverUser)
-type VerifyUser   = VerifyReq   -> IO (Either Text User)
+type RegisterUser = RegisterReq -> IO (Either AvtorError UnverUser)
+type VerifyUser   = VerifyReq   -> IO (Either AvtorError User)
 
-type Login  = LoginReq  -> IO (Either Text AuthToken)
-type Logout = LogoutReq -> IO (Either Text ())
+type Login  = LoginReq  -> IO (Either AvtorError AuthToken)
+type Logout = LogoutReq -> IO (Either AvtorError ())
 
 type Authenticate = AuthReq -> IO (Maybe LoggedInUser)
 
+
+data AvtorError
+  = AccountNotFound
+  | AccountAlreadyExists
+  | EmailInvalid
+  | PasswordInvalid
+  | ConfirmPasswordDoesNotMatch
+  | PasswordIncorrect
+  | UserNotFound
+  | UserExists
+  | RepoError
+  | IpRestricted
+  | LoginAttemptsExceeded
 
 newtype AccountId
   = AccountId
@@ -134,7 +147,8 @@ data RestrictedIp =
   }
   deriving (Show)
 
-(...) =  flip ($)
+
+(...) = flip ($)
 
 
 data CreateAccountReq
@@ -142,14 +156,14 @@ data CreateAccountReq
   { createAccountReqName    :: Text
   , createAccountReqGenUUID :: IO UUID
   , findAccountByName       :: Text -> IO (Maybe Account)
-  , insertAccount           :: Account -> IO (Either Text ())
+  , insertAccount           :: Account -> IO (Either AvtorError ())
   }
 
 createAccount :: CreateAccount
 createAccount req@CreateAccountReq{..} = do
   maybeAccount <- findAccountByName createAccountReqName
   case maybeAccount of
-    Just _  -> return $ Left "Account Exists"
+    Just _  -> return $ Left UserExists
     Nothing -> do
       uuid <- createAccountReqGenUUID
       let account = Account (AccountId uuid) createAccountReqName
@@ -162,20 +176,20 @@ createAccount req@CreateAccountReq{..} = do
 data UpdateAccountReq
   = UpdateAccountReq
   { modifiedAccount                :: Account
-  , updateAccountReqFindById       :: AccountId -> IO (Either Text (Maybe Account))
-  , findOthersByName               :: AccountId -> Text -> IO (Either Text (Maybe Account))
-  , updateAccountReqUpdateAccount  :: Account -> IO (Either Text ())
+  , updateAccountReqFindById       :: AccountId -> IO (Either AvtorError (Maybe Account))
+  , findOthersByName               :: AccountId -> Text -> IO (Either AvtorError (Maybe Account))
+  , updateAccountReqUpdateAccount  :: Account -> IO (Either AvtorError ())
   }
 
 updateAccount :: UpdateAccount
 updateAccount req@UpdateAccountReq{..} = runExceptT $ do
   maybeAccount <- ExceptT $ updateAccountReqFindById $ modifiedAccount..._accountId
   case maybeAccount of
-    Nothing -> throwE "Account does not exist"
+    Nothing -> throwE AccountNotFound
     Just _  -> do
       maybeOtherAccount <- ExceptT $ findOthersByName (modifiedAccount..._accountId) (modifiedAccount...accountName)
       case maybeOtherAccount of
-        Just _  -> throwE "Account by this name currently exists"
+        Just _  -> throwE AccountAlreadyExists
         Nothing -> do
           _ <- ExceptT $ updateAccountReqUpdateAccount modifiedAccount
           return modifiedAccount
@@ -187,26 +201,26 @@ data RegisterReq
   , registerReqAccountId :: UUID
   , generateUUID :: IO UUID
   , findByEmail  :: Text      -> IO (Maybe User)
-  , savePreUser  :: UnverUser -> IO (Either Text ())
-  , sendEmail    :: Text      -> IO (Either Text ())
+  , savePreUser  :: UnverUser -> IO (Either AvtorError ())
+  , sendEmail    :: Text      -> IO (Either AvtorError ())
   }
 
 registerUser :: RegisterUser
 registerUser req@RegisterReq{..} = do
   if regDto...rPass /= regDto...rConfirmPass
     then
-      return $ Left "Passwords do not match"
+      return $ Left ConfirmPasswordDoesNotMatch
     else do
       mayUser <- findByEmail $ regDto...rEmail
       case mayUser of
-        Just _  -> return $ Left "User Exists"
+        Just _  -> return $ Left UserExists
         Nothing -> runExceptT $ do
           emailSentRes <- ExceptT $ sendEmail $ regDto...rEmail
           uuid         <- liftIO generateUUID
           token        <- liftIO generateUUID
           let unUser    = mapRegistrationDataToUnverifiedUser uuid token registerReqAccountId regDto
           savedUser    <- ExceptT $ savePreUser $ unUser
-          return $ 
+          return $
             unUser
 
 
@@ -214,14 +228,14 @@ data VerifyReq
   = VerifyReq
   { vToken              :: VerToken
   , findPreuserByToken  :: VerToken  -> IO (Maybe UnverUser)
-  , saveUser            :: User      -> IO (Either Text User)
+  , saveUser            :: User      -> IO (Either AvtorError User)
   }
 
 verifyUser :: VerifyUser
 verifyUser req = do
   mayUnverifiedUser <- req...findPreuserByToken $ req...vToken
   case mayUnverifiedUser of
-    Nothing -> return $ Left "Unverified user could not be found"
+    Nothing -> return $ Left UserNotFound
     Just u  -> runExceptT $ do
       let newUser = mapUnverifiedUserToUser u
       ExceptT $ req...saveUser $ newUser
@@ -231,47 +245,50 @@ data LoginReq =
   LoginReq
   { loginReqDto          :: LoginDto
   , ipAddress            :: Text
-  , findRestrictedIp     :: Text -> IO (Either Text (Maybe RestrictedIp))
-  , findAttempts         :: Text -> IO (Either Text [LoginAttempt])
-  , findUserByEmail      :: Text -> IO (Either Text (Maybe User))
+  , findRestrictedIp     :: Text -> IO (Either AvtorError (Maybe RestrictedIp))
+  , findAttempts         :: Text -> IO (Either AvtorError [LoginAttempt])
+  , findUserByEmail      :: Text -> IO (Either AvtorError (Maybe User))
   , matchPassword        :: Text -> Text -> Bool
   , createDate           :: IO UTCTime
-  , insertAttempt        :: LoginAttempt -> IO (Either Text ())
+  , insertAttempt        :: LoginAttempt -> IO (Either AvtorError ())
   , loginReqGenerateUUID :: IO UUID
   }
 
 login :: Login
 login req@LoginReq{..} = runExceptT $ do
-  maybeBadIp    <- ExceptT $ findRestrictedIp ipAddress
+  maybeBadIp <- ExceptT $ findRestrictedIp ipAddress
   case maybeBadIp of
-    Just _ -> throwE "Restricted Ip Attempt"
+    Just _ -> throwE IpRestricted
     Nothing -> do
       loginAttempts <- ExceptT $ findAttempts ipAddress
-      guard $ (Prelude.length loginAttempts) > 10 -- figure out how to throw error here
-      userOpt       <- ExceptT $ findUserByEmail $ loginReqDto...loginDtoEmail
-      case userOpt of
-        Nothing -> do
-          attemptId <- liftIO loginReqGenerateUUID
-          now       <- liftIO createDate
-          let attempt = LoginAttempt (LoginAttemptId attemptId) ipAddress now
-          _ <- ExceptT $ insertAttempt attempt
-          throwE "User not found"
-        Just u  ->
-          if matchPassword (loginReqDto...loginDtoPass) (u...userPass)
-            then
-              return $ AuthToken "todo"
-            else do
+      if Prelude.length loginAttempts > 10
+        then
+          throwE LoginAttemptsExceeded
+        else do
+          userOpt <- ExceptT $ findUserByEmail $ loginReqDto...loginDtoEmail
+          case userOpt of
+            Nothing -> do
               attemptId <- liftIO loginReqGenerateUUID
               now       <- liftIO createDate
               let attempt = LoginAttempt (LoginAttemptId attemptId) ipAddress now
               _ <- ExceptT $ insertAttempt attempt
-              throwE "Passwords do not match"
+              throwE UserNotFound
+            Just u  ->
+              if matchPassword (loginReqDto...loginDtoPass) (u...userPass)
+                then
+                  return $ AuthToken "todo"
+                else do
+                  attemptId <- liftIO loginReqGenerateUUID
+                  now       <- liftIO createDate
+                  let attempt = LoginAttempt (LoginAttemptId attemptId) ipAddress now
+                  _ <- ExceptT $ insertAttempt attempt
+                  throwE PasswordIncorrect
 
 
 
 data LogoutReq = LogoutReq
   { logoutReqAuthToken :: AuthToken
-  , destroyAuthToken :: AuthToken -> IO (Either Text ())
+  , destroyAuthToken :: AuthToken -> IO (Either AvtorError ())
   }
 
 logout :: Logout
